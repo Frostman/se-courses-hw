02cd70d062475f96a8b351f7b32351bb6e56c2fc
Frostman <me@frostman.ru>
2011-04-06 16:14:03 +0400

    spellcheck ready; => hw_3 and hw_4 fully ready.

--- hw_3/spellcheck.c
+++ hw_3/spellcheck.c
@@ -5,13 +5,89 @@
 
 int main() {
   char buf[1024];
+  char word[1024];
   char prefix[1024];
   char suffix[1024];
-  char replace[1024];
+  char insert[1024];
+  char correct[1024];
+  char rem_chars[1024];
+
+  prefix[0] = 0;
+  suffix[0] = 0;
+  insert[0] = 0;
+  correct[0] = 0;
+  rem_chars[0] = 0;
 
   while(1 == scanf("%1023s", &buf)) {
     int len = strlen(buf);
-    printf("read: %s (%d)\n", buf, len);
+
+    if(buf[0] == '$') {
+      strcpy(suffix, buf + 1);
+      //printf("suffix: %s\n", suffix);
+    } else if(buf[0] == '&') {
+      strcpy(prefix, buf + 1);
+      //printf("prefix: %s\n", prefix);
+    } else if(buf[0] == '.') {
+      strcpy(insert, buf + 1);
+      //printf("insert: %s\n", insert);
+    } else if(buf[0] == '-') {
+      strcpy(correct, buf + 1);
+      //printf("correct: %s\n", correct);
+    } else if(buf[0] == '!') {
+      strcpy(rem_chars, buf + 1);
+      //printf("rem_chars: %s\n", rem_chars);
+    } else {
+      //printf("read: %s ; ", buf);
+      
+      word[0] = 0;
+      int shift = 0, add_suffix = 0;
+      if(buf[0] == '^') {
+        strcat(word, prefix);
+        shift = 1;
+      }
+      if(buf[len - 1] == '$') {
+        buf[len - 1] = 0;
+        add_suffix = 1;
+      }
+      strcat(word, buf + shift);
+
+      if(1 == add_suffix) {
+        strcat(word, suffix);
+      }
+
+      char * dot_idx = strchr(word, '.');
+
+      if (NULL != dot_idx) {
+        shift = dot_idx - word + 1;
+        strcpy(buf, word);
+        *(buf + shift - 1) = 0;
+        word[0] = 0;
+        strcat(word, buf);
+        strcat(word, insert);
+        strcat(word, buf + shift);
+      }
+
+      for(;;) {
+        char * bad_idx = strpbrk(word, rem_chars);
+        if(NULL == bad_idx) {
+          break;
+        }
+        shift = bad_idx - word + 1;
+        strcpy(buf, word);
+        *(buf + shift - 1) = 0;
+        word[0] = 0;
+        strcat(word, buf);
+        strcat(word, buf + shift);
+      }
+      
+      //printf("check: %s\n", word);
+
+      if(0 == strcmp(correct, word) || is_spelled_correctly(word)){
+        printf("%s: correct\n", word);
+      } else {
+        printf("%s: incorrect\n", word);
+      }
+    }
   }
 
   return EXIT_SUCCESS;
---
+++ hw_4/flip-image-optimized.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pixel_s {
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+} pixel_t;
+
+int main() {
+
+  scanf("P3\n");
+
+  int width, height, max_val, r, g, b;
+  scanf("%d %d %d", &width, &height, &max_val);
+
+  pixel_t ** array = (pixel_t **) malloc(height * sizeof(pixel_t *));
+  pixel_t * p;
+  for(int i = 0; i < height; i++) {
+    array[i] = (pixel_t *) malloc(width * sizeof(pixel_t));
+    
+    for(int j = 0; j < width; j++) {
+      p = &array[i][j];
+      scanf("%d %d %d", &r, &g, &b);
+      p->r = r;
+      p->g = g;
+      p->b = b;
+    }
+  }
+
+  printf("P3\n%d %d\n%d\n", width, height, max_val);
+  
+  for(int i = height - 1; i >= 0; i--) {
+    for(int j = 0; j < width; j++) {
+      p = &array[i][j];
+      printf("%d %d %d ", p->r, p->g, p->b); 
+    }
+    printf("\n");
+
+    free(array[i]);
+  }
+  free(array);
+  
+  return EXIT_SUCCESS;
+}
+
